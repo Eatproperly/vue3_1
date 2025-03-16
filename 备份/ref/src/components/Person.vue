c5983c12e7bc302d5cd958b7d8fd765b0dc8db4c
L <1485085813@qq.com>
2025-03-16 09:37:57 +0800

    Initial commit

---
+++ 备份/ref/src/components/Person.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="showTel">查看联系方式</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changeName">修改姓名</button>
+    </div>
+</template>
+
+<script lang="ts">
+  export default{
+    name : 'Person',
+  }
+</script>
+
+<script lang="ts" setup name = "Person">
+import { ref } from 'vue'
+    let name = ref('zhangsan')
+    let age  = ref(18)
+    let tel = 1388888888
+    //方法
+    function changeName(){
+      name.value = 'zhang-san'
+      console.log(name)
+    }
+    function changeAge(){
+      age.value += 1
+      console.log(age)
+    }
+    function showTel(){
+        alert(tel)
+    }
+</script>
+
+<style>
+    .person{
+      background-color: aquamarine;
+      box-shadow: 0 0 10px;
+      border-radius: 10px;
+      padding: 20px;
+    }
+</style>
---
+++ 备份/setup语法糖/src/components/Person.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="showTel">查看联系方式</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="changeName">修改姓名</button>
+    </div>
+</template>
+
+<script lang="ts">
+  export default{
+    name : 'Person',
+  }
+</script>
+
+<script lang="ts" setup name = "Person">
+    let name = 'zhangsan'
+    let age  = 18
+    let tel = 1388888888
+    //方法
+    function changeName(){
+      name = 'zhang-san'
+      console.log(name)
+    }
+    function changeAge(){
+      age += 1
+      console.log(age)
+    }
+    function showTel(){
+        alert(tel)
+    }
+</script>
+
+<style>
+    .person{
+      background-color: aquamarine;
+      box-shadow: 0 0 10px;
+      border-radius: 10px;
+      padding: 20px;
+    }
+</style>
